73b783120405c061d05825b0d19384d513f8ea23
marklovefap <46591069+marklovefap@users.noreply.github.com>
2020-03-24 22:14:55 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {v4} from 'uuid';
 import axios from 'axios';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import firebase from './firebase/firebase'
 import Header from './components/Header';
 import AddTransaction from './components/AddTransaction';
 import Balance from './components/Balance';
@@ -51,11 +51,18 @@ export default class App extends Component {
 
   loadJsonData = () => {
     // get data from json file: "public/static/data.json"
-    axios.get('/static/data.json')
+    /*axios.get('/static/data.json')
       .then( res => {
         const data = res.data;
         this.setState( { transactions: data } );
-      });
+      }); */
+      firebase.firestore().collection('data').onSnapshot(items => {
+        const transaction = [] //array type
+        items.forEach( res => {
+          transaction.push(res.data())
+        })
+        this.setState({transactions:transaction})
+      })
   }
 
   componentDidMount() {
@@ -71,7 +78,12 @@ export default class App extends Component {
     } else if ( !isNaN(name)) {
       window.alert('Please fill only TEXT detail in transaction name.');
       return false;
-    } else if (+amount === 0) {
+    } 
+    else if(!Number.isInteger(+amount)){
+      window.alert('Please fill only Integer in transaction amount field.');
+      return false;
+    }
+    else if (+amount === 0) {
       window.alert('Amount CANNOT be zero!');
       return false;
     }
@@ -89,9 +101,10 @@ export default class App extends Component {
       id: v4(),
       name,
       amount: +amount,
-      date: new Date()
+      date: new Date().getTime()
     }
 
+    firebase.firestore().collection('data').add(newTransaction)
     this.state.transactions.unshift(newTransaction);
     this.setState( { transactions: this.state.transactions } );
   }
@@ -99,6 +112,11 @@ export default class App extends Component {
   clearTransactions = () => {
     let ans = window.confirm("You are going to clear all transaction history!!!")
     if (ans) {
+      firebase.firestore().collection('data').get().then(function(Snapshot){
+        Snapshot.forEach(function(doc){
+          doc.ref.delete()
+        })
+      })
       this.setState( { transactions: [] } );
     }
   }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export default function About() {
     <div>
       <div className="container mt-5">
         <div className="card">
-          <img src={process.env.PUBLIC_URL+'/andreas-klassen.jpg'} className='card-img-top' alt="..." />
+          <img src={'https://firebasestorage.googleapis.com/v0/b/lab8-con.appspot.com/o/hokp.jpg?alt=media&token=72348955-66f8-403a-9e84-7c88abfeb0f0'} className='card-img-top' alt="..." />
           <div className="card-body">
             <h5 className="card-title">Developer information</h5>
-            <p>Dome Potikanond (610610999)</p>
+            <p>Chutipon Vimonkanjana (610610578)</p>
             <p>This app use Google Firebase as backend.</p>
             <Link to="/">
               <h3>
